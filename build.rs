9704c51f4074b5c0b6862eb972095ab5aea92894
cristian-vogel <cristian@crabnebula.dev>
2026-04-01 21:26:38 +0200

    doc: aim for CI docs only

--- build.rs
+++ build.rs
@@ -1,4 +1,12 @@
 fn main() {
+    if std::env::var_os("ELEM_AUDIO_DOCS_ONLY").is_some() {
+        return;
+    }
+
+    let manifest_dir =
+        std::env::var("CARGO_MANIFEST_DIR").expect("CARGO_MANIFEST_DIR is set by Cargo");
+    let vendor_runtime = format!("{manifest_dir}/src/vendor/elementary/runtime");
+
     println!("cargo:rerun-if-changed=src/ffi/elementary_bridge.cpp");
     println!("cargo:rerun-if-changed=src/vendor/elementary/runtime");
     println!("cargo:rerun-if-env-changed=CARGO_FEATURE_RESOURCES");
@@ -7,7 +15,7 @@ fn main() {
     build
         .cpp(true)
         .file("src/ffi/elementary_bridge.cpp")
-        .include("src/vendor/elementary/runtime")
+        .include(&vendor_runtime)
         .flag_if_supported("-std=c++17");
 
     if std::env::var_os("CARGO_FEATURE_RESOURCES").is_some() {
